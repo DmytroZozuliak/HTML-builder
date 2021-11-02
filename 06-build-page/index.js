6f651d474bd96da79c439f5e571760bd4fc5b99b
DmytroZozuliak <dmytro.zozuliak@gmail.com>
2021-11-02 11:16:13 +0200

    feat: 6 task, assets + style.css

--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -2,18 +2,42 @@ const fs = require('fs');
 const path = require('path');
 const cssDir = path.join(__dirname, 'styles');
 const distFile = path.join(__dirname, 'project-dist', 'style.css');
+const FSP = require('fs').promises;
 const assets = path.join(__dirname, 'assets');
 const copyAssets = path.join(__dirname, 'project-dist', 'assets');
 
+// создаем папку 'project-dist' и вкладываем в нее функции создания Цсс
 fs.mkdir(path.join(__dirname, 'project-dist'), { recursive: true }, err => {
   if (err) console.log('Ошибка в создании папки');
-  //   createFileCss();
+  createFileCss();
 });
 
+// проверяем наличие папки copyAssets
+fs.access(copyAssets, err => {
+  if (err) {
+    console.log('creating folder...');
+    copyAsset(assets, copyAssets);
+  } else {
+    console.log('updating folder...');
+
+    //  Перед копирыванием удаляем все содержимое папок и файлов, затем заново копируем
+    fs.rmdir(
+      path.join(copyAssets),
+      {
+        recursive: true,
+        force: true,
+      },
+      () => {
+        copyAsset(assets, copyAssets);
+      }
+    );
+  }
+});
+
+// Функция создание единого цсс файла в 'project-dist'
 function createFileCss() {
   fs.readdir(cssDir, (err, files) => {
     if (err) console.log('Ошибка чтения папки (1)');
-    //   console.log(files);
 
     let sorted = files.filter(file => path.extname(file) === '.css');
 
@@ -35,11 +59,24 @@ function createFileCss() {
   });
 }
 
-// function сcopyAssets() {}
+// Функция копирования ассетов
+async function copyAsset(src, dest) {
+  const entries = await FSP.readdir(src, { withFileTypes: true });
+  await FSP.mkdir(dest);
+  for (let entry of entries) {
+    const srcPath = path.join(src, entry.name);
+    const destPath = path.join(dest, entry.name);
+    if (entry.isDirectory()) {
+      // console.log('copied folder');
+      await copyAsset(srcPath, destPath);
+    } else {
+      // console.log('copied file');
+      await FSP.copyFile(srcPath, destPath);
+    }
+  }
+}
 
-сcopyAssets(path.join(__dirname, 'assets'));
-
-function сcopyAssets(pathToFiles) {
+/* function сcopyAssets(pathToFiles) {
   fs.readdir(pathToFiles, (err, arrFiles) => {
     if (err) console.log('Ошибка чтения папки (3)');
 
@@ -83,65 +120,4 @@ function сcopyAssets(pathToFiles) {
     //  }
     //  console.log('folder was copied');
   });
-}
-
-// todo without recursion
-
-// fs.readdir(assetsCopy, (err2, files2) => {
-//   if (err2) {
-//     console.log(err2);
-//   }
-//   fs.readdir(assets, (err, files) => {
-//     if (err) {
-//       console.log(err);
-//     }
-//     files2.forEach(item => {
-//       if (!files.includes(item)) {
-//         fs.unlink(path.join(assetsCopy, `${item}`), err => {
-//           if (err) throw err;
-//         });
-//       }
-//     });
-//     files.forEach(dir => {
-//       fs.mkdir(
-//         path.join(assetsCopy, `${dir}`),
-//         {
-//           recursive: true,
-//         },
-//         err => {
-//           if (err) {
-//             return console.error(err);
-//           }
-//           fs.readdir(path.join(assetsCopy, `${dir}`), (err2, item2) => {
-//             if (err2) {
-//               console.log(err);
-//             }
-//             fs.readdir(path.join(assets, `${dir}`), (err1, item) => {
-//               if (err1) {
-//                 console.log(err);
-//               }
-//               item2.forEach(file => {
-//                 if (!item.includes(file)) {
-//                   fs.unlink(path.join(assetsCopy, `${dir}`, `${file}`), err => {
-//                     if (err) throw err;
-//                   });
-//                 }
-//               });
-//               item.forEach(file => {
-//                 fs.copyFile(
-//                   path.join(assets, `${dir}`, `${file}`),
-//                   path.join(assetsCopy, `${dir}`, `${file}`),
-//                   err => {
-//                     if (err) {
-//                       console.log('Error Found:', err);
-//                     }
-//                   }
-//                 );
-//               });
-//             });
-//           });
-//         }
-//       );
-//     });
-//   });
-// });
+} */
